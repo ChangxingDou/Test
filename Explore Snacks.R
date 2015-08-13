a257fd8a7b4c77de25bb361688a921b3becd03b5
Alex Dou <alex.dou@nielsen.com>
2015-08-13 11:09:40 -0400

    UPD
    DUP

---
+++ Explore Snacks.R
@@ -0,0 +1,9 @@
+snacks_data = read.csv("C:\\Users\\doual01\\Desktop\\Dataset\\Analysis-Snacks_R.csv",header = T)
+dim(snacks_data)
+head(snacks_data)
+names(snacks_data)
+lm_test = lm(snacks_data$Person.Channel.Reach..~
+               as.factor(snacks_data$Gender...Age)+snacks_data$HHSize+
+               as.factor(snacks_data$Kids)+as.factor(snacks_data$Social.Class))
+attach(snacks_data)
+detach(snacks_data)
